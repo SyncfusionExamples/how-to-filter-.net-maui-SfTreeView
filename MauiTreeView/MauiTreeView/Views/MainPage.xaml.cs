178bc7fe21e24107ff8a248c3e5f3c7d18b17d11
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2024-04-16 17:52:09 +0530

    Added sample

---
+++ MauiTreeView/MauiTreeView/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTreeView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
